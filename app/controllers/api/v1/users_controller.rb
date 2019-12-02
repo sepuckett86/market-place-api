51a822c09488392793b90f2a257d3b784fbefe2a
Susan Puckett <sepuckett86@gmail.com>
2019-12-02 15:38:32 -0800

    token etc

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::UsersController < ApplicationController
   before_action :set_user, only: %i[show update destroy]
   
   def show
-    render json: User.find(params[:id])
+    render json: @user
   end
 
   def create
--- test/controllers/api/v1/users_controller_test.rb
+++ test/controllers/api/v1/users_controller_test.rb
@@ -8,7 +8,6 @@ class Api::V1::UsersControllerTest < ActionDispatch::IntegrationTest
   test "should show user" do
     get api_v1_user_url(@user), as: :json
     assert_response :success
-    # Test to ensure response contains the correct email
     json_response = JSON.parse(self.response.body)
     assert_equal @user.email, json_response['email']
   end
